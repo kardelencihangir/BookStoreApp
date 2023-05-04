746b65d643661a0f17d5943baf06e3fab490ff2c
Kardelen Cihangir <kardelencihangir@gmail.com>
2023-05-04 13:53:08 +0300

    Rol tanımları ve migration'ları tamamlandı.

--- Repositories/EFCore/RepositoryContext.cs
+++ Repositories/EFCore/RepositoryContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Repositories.EFCore.Config;
+using System.Reflection;
 
 namespace Repositories.EFCore
 {
@@ -17,7 +18,10 @@ namespace Repositories.EFCore
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.ApplyConfiguration(new BookConfig());
+            //modelBuilder.ApplyConfiguration(new BookConfig());
+            //modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
         }
     }
 }
